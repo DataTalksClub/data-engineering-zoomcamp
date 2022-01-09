e89b32bf2158effb40efd7c2a0cf4b5704828bc4
victoriapm <me.victoria@gmail.com>
2022-01-09 13:25:05 +0100

    remove legacy code

--- week_5_analytics_engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ week_5_analytics_engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -2,17 +2,17 @@
 
 
 select
-    dispatching_base_num text COLLATE pg_catalog."default",
-    pickup_datetime timestamp without time zone,
-    dropoff_datetime timestamp without time zone,
-    pulocationid integer,
-    dolocationid integer,
-    sr_flag integer,
-    affiliated_base_number text COLLATE pg_catalog."default"
+    dispatching_base_num::text COLLATE pg_catalog."default",
+    pickup_datetime::timestamp without time zone,
+    dropoff_datetime::timestamp without time zone,
+    pulocationid::integer,
+    dolocationid::integer,
+    sr_flag::integer,
+    affiliated_base_number::text COLLATE pg_catalog."default"
 
 from {{ source('staging','fhv_tripdata_2021_01') }}
 
--- dbt run --model <model.sql> --var 'is_test_run: false'
+-- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
 
   limit 100
