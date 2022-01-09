64a8ac9ce2725c70416b753fd2aea8761df234de
victoriapm <me.victoria@gmail.com>
2022-01-09 20:42:40 +0100

    enhance staging models
    Unifies column names

--- week_5_analytics_engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ week_5_analytics_engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -3,20 +3,22 @@
 
 select
     -- identifiers
+    {{ dbt_utils.surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
     vendorid::integer,
     ratecodeid::integer,
-    pulocationid::integer,
-    dolocationid::integer,
+    pulocationid::integer as pickup_locationid,
+    dolocationid::integer as dropoff_locationid,
     
     -- timestamps
-    lpep_pickup_datetime::timestamp without time zone,
-    lpep_dropoff_datetime::timestamp without time zone,
+    tpep_pickup_datetime::timestamp without time zone as pickup_datetime,
+    tpep_dropoff_datetime::timestamp without time zone as dropoff_datetime,
     
     -- trip info
-    store_and_fwd_flag text COLLATE pg_catalog."default",
+    store_and_fwd_flag::varchar,
     passenger_count::integer,
     trip_distance::double precision,
-    trip_type::integer,
+    -- yellow cabs are always street-hail
+    1 as trip_type,
     
     -- payment info
     fare_amount::double precision,
@@ -24,12 +26,15 @@ select
     mta_tax::double precision,
     tip_amount::double precision,
     tolls_amount::double precision,
-    ehail_fee::integer,
+    0 as ehail_fee,
     improvement_surcharge::double precision,
     total_amount::double precision,
     payment_type::integer,
+    {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     congestion_surcharge::double precision
-from {{ source('staging','green_tripdata_2021_01') }}
+from {{ source('staging','yellow_tripdata_2021_01') }}
+where vendorid is not null 
+  -- qualify row_number() over(partition by tripid) = 1
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
