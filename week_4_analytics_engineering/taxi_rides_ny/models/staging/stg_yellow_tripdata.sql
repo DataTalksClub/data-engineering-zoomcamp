168ad4741be2409a1b476264102b5625b3e96609
victoriapm <me.victoria@gmail.com>
2022-02-05 02:24:22 +0100

    adapt sql to fit both bq and pg

--- week_4_analytics_engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ week_4_analytics_engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -2,36 +2,36 @@
 
 
 select
-    -- identifiers
+   -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
-    vendorid::integer,
-    ratecodeid::integer,
-    pulocationid::integer as pickup_locationid,
-    dolocationid::integer as dropoff_locationid,
+    cast(vendorid as integer) as vendorid,
+    cast(ratecodeid as integer) as ratecodeid,
+    cast(pulocationid as integer) as  pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
     
     -- timestamps
-    tpep_pickup_datetime::timestamp without time zone as pickup_datetime,
-    tpep_dropoff_datetime::timestamp without time zone as dropoff_datetime,
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
     
     -- trip info
-    store_and_fwd_flag::varchar,
-    passenger_count::integer,
-    trip_distance::double precision,
+    cast(store_and_fwd_flag as varchar) as store_and_fwd_flag,
+    cast(passenger_count as integer) as passenger_count,
+    cast(trip_distance as double precision) as trip_distance,
     -- yellow cabs are always street-hail
     1 as trip_type,
     
     -- payment info
-    fare_amount::double precision,
-    extra::double precision,
-    mta_tax::double precision,
-    tip_amount::double precision,
-    tolls_amount::double precision,
-    0 as ehail_fee,
-    improvement_surcharge::double precision,
-    total_amount::double precision,
-    payment_type::integer,
+    cast(fare_amount as double precision) as fare_amount,
+    cast(extra as double precision) as extra,
+    cast(mta_tax as double precision) as mta_tax,
+    cast(tip_amount as double precision) as tip_amount,
+    cast(tolls_amount as double precision) as tolls_amount,
+     0 as ehail_fee,
+    cast(improvement_surcharge as double precision) as improvement_surcharge,
+    cast(total_amount as double precision) as total_amount,
+    cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description, 
-    congestion_surcharge::double precision
+    cast(congestion_surcharge as double precision) as congestion_surcharge
 from {{ source('staging','yellow_tripdata_2021_01') }}
 where vendorid is not null 
   -- qualify row_number() over(partition by tripid) = 1
