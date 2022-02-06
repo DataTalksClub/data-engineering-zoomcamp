7413f611a25d93af75996cd464354d8184b71b13
ibraheemtaha <ibraheem.taha@alectio.com>
2022-02-06 20:54:28 +0200

    add try-except block

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -37,19 +37,24 @@ def main(params):
 
 
     while True: 
-        t_start = time()
 
-        df = next(df_iter)
+        try:
+            t_start = time()
+            
+            df = next(df_iter)
 
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-        df.to_sql(name=table_name, con=engine, if_exists='append')
+            df.to_sql(name=table_name, con=engine, if_exists='append')
 
-        t_end = time()
+            t_end = time()
 
-        print('inserted another chunk, took %.3f second' % (t_end - t_start))
+            print('inserted another chunk, took %.3f second' % (t_end - t_start))
 
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
