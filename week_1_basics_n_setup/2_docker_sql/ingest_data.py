75b397a598e8ff3c653d0a4d591d60761cd21ad1
ibraheemtaha <ibraheem.taha@alectio.com>
2022-02-06 17:30:34 +0200

    use wget direcly, improve the exit of while loop by adding try-except block

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
-import argparse
-
-from time import time
-
+import wget
 import pandas as pd
 from sqlalchemy import create_engine
+from time import time
 
+import argparse
 
 def main(params):
-    user = params.user
-    password = params.password
-    host = params.host 
-    port = params.port 
-    db = params.db
+    user=params.user
+    password=params.password
+    host=params.host
+    port=params.port
+    db=params.db
     table_name = params.table_name
     url = params.url
-    csv_name = 'output.csv'
 
-    os.system(f"wget {url} -O {csv_name}")
+    csv_name = wget.download(url)
+
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    engine.connect()
 
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
-
     df = next(df_iter)
+
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
@@ -35,32 +34,36 @@ def main(params):
 
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
+    while True:
 
-    while True: 
-        t_start = time()
+        try:
+            t_start = time()
 
-        df = next(df_iter)
+            df = next(df_iter)
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            df.to_sql(name=table_name, con=engine, if_exists='append')
 
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            t_end = time()
 
-        df.to_sql(name=table_name, con=engine, if_exists='append')
+            print('inserted another chunk... which took %.3f second' % (t_end - t_start))
 
-        t_end = time()
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
 
-        print('inserted another chunk, took %.3f second' % (t_end - t_start))
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres.')
 
-    parser.add_argument('--user', required=True, help='user name for postgres')
-    parser.add_argument('--password', required=True, help='password for postgres')
-    parser.add_argument('--host', required=True, help='host for postgres')
-    parser.add_argument('--port', required=True, help='port for postgres')
-    parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='db name for postgres')
+    parser.add_argument('--table_name', help='the name of the table where the results will be written to')
+    parser.add_argument('--url', help='url of the csv file')
 
     args = parser.parse_args()
 
