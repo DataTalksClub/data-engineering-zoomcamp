64bf1803d59c967fa20aa790a598d7ab04cd7b15
Keith Holliday <krh121791@yahoo.com>
2022-08-02 21:40:10 -0600

    [203] Revert parquet ingest to stay consistent with video (#206)

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import argparse
 import os
-import pyarrow.parquet as pq
+import argparse
+
+from time import time
+
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -16,20 +18,46 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
-    filename = 'output.parquet'
-    
-    os.system(f"wget {url} -O {filename}")
+    csv_name = 'output.csv'
+
+    os.system(f"wget {url} -O {csv_name}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-    
-    parquet_table = pq.read_table(filename)
-    df = parquet_table.to_pandas()
 
-    df.to_sql(name="yellow_taxi_data", con=engine, if_exists='append', chunksize=100000)
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+
+    while True: 
+
+        try:
+            t_start = time()
+            
+            df = next(df_iter)
+
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+            df.to_sql(name=table_name, con=engine, if_exists='append')
+
+            t_end = time()
+
+            print('inserted another chunk, took %.3f second' % (t_end - t_start))
+
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Ingest data to Postgres')
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
 
     parser.add_argument('--user', required=True, help='user name for postgres')
     parser.add_argument('--password', required=True, help='password for postgres')
@@ -37,7 +65,7 @@ if __name__ == '__main__':
     parser.add_argument('--port', required=True, help='port for postgres')
     parser.add_argument('--db', required=True, help='database name for postgres')
     parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the parquet file')
+    parser.add_argument('--url', required=True, help='url of the csv file')
 
     args = parser.parse_args()
 
