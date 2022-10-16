823120f7d5467f38f61f6c527e3d8e03754aa5ca
Asim Zahid <asimzahid02@gmail.com>
2022-10-16 12:00:06 +0500

    Clean Code and Prod grade
    Upgraded the code to clean OOP format and production-grade

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -3,76 +3,112 @@
 
 import os
 import argparse
-
 from time import time
-
 import pandas as pd
 from sqlalchemy import create_engine
+from urllib.parse import urlparse
 
 
-def main(params):
-    user = params.user
-    password = params.password
-    host = params.host 
-    port = params.port 
-    db = params.db
-    table_name = params.table_name
-    url = params.url
-    
-    # the backup files are gzipped, and it's important to keep the correct extension
-    # for pandas to be able to open the file
-    if url.endswith('.csv.gz'):
-        csv_name = 'output.csv.gz'
-    else:
-        csv_name = 'output.csv'
+class IngestData(object):
+    """Ingestion data pipeline
 
-    os.system(f"wget {url} -O {csv_name}")
+    Args:
+        object (_type_): _description_
+    """
 
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    def __init__(self, params):
+        # self.params = params
+        self.user = params.user
+        self.password = params.password
+        self.host = params.host
+        self.port = params.port
+        self.db = params.db
+        self.table_name = params.table_name
+        self.url = params.url
+        self.df_iter = self.download_read_csv()
+        self.engine = self.create_db_conn()
 
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    def download_read_csv(self):
+        """Download CSV file read and return dataframe iter"""
+        # the backup files are gzipped, and it's important to keep the correct extension
+        # for pandas to be able to open the file
+        file_url = urlparse(self.url)
+        if self.url.endswith(".csv.gz"):
+            csv_name = "output.csv.gz"
+        else:
+            csv_name = "output.csv"
 
-    df = next(df_iter)
+        os.system(f"wget {self.url} -o {csv_name}")
+        csv_name = os.path.basename(file_url.path)
+        print(csv_name)
+        return pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    def transformation(self, df):
+        """dataframe transformation."""
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        return df
 
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    def create_db_conn(self):
+        """Create DB connection
 
-    df.to_sql(name=table_name, con=engine, if_exists='append')
+        Returns:
+            _type_: postgresql DB connection
+        """
+        engine = create_engine(
+            f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
+        )
+        return engine
+
+    def create_schema(self):
+        """Create SQL DB schema"""
+        df = next(self.df_iter)
+        df = self.transformation(df)
+        df.head(n=0).to_sql(name=self.table_name, con=self.engine, if_exists="replace")
+        # df.to_sql(name=self.table_name, con=self.engine, if_exists="append")
+
+    def pipeline(self, df):
+        """Data Ingestion Pipeline
+
+        Args:
+            df (_type_): _description_
+        """
+        while True:
+            try:
+                t_start = time()
+                df = next(self.df_iter)
+
+                df = self.transformation(df)
+
+                df.to_sql(name=self.table_name, con=self.engine, if_exists="append")
+
+                t_end = time()
+
+                print("inserted another chuck, took %.3f second" % (t_end - t_start))
+
+            except StopIteration:
+                print("Finished ingesting data into the postgres database")
+                break
 
 
-    while True: 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Ingest CSV data to Postgres")
 
-        try:
-            t_start = time()
-            
-            df = next(df_iter)
-
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-            df.to_sql(name=table_name, con=engine, if_exists='append')
-
-            t_end = time()
-
-            print('inserted another chunk, took %.3f second' % (t_end - t_start))
-
-        except StopIteration:
-            print("Finished ingesting data into the postgres database")
-            break
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
-
-    parser.add_argument('--user', required=True, help='user name for postgres')
-    parser.add_argument('--password', required=True, help='password for postgres')
-    parser.add_argument('--host', required=True, help='host for postgres')
-    parser.add_argument('--port', required=True, help='port for postgres')
-    parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument("--user", required=True, help="user name for postgres")
+    parser.add_argument("--password", required=True, help="password for postgres")
+    parser.add_argument("--host", required=True, help="host for postgres")
+    parser.add_argument("--port", required=True, help="port for post")
+    parser.add_argument("--db", required=True, help="database name for postgres")
+    parser.add_argument(
+        "--table_name",
+        required=True,
+        help="name of table where we will write the results to",
+    )
+    parser.add_argument("--url", required=True, help="url of the csv file")
 
     args = parser.parse_args()
 
-    main(args)
+    # main(args)
+    ingestion_pipeline = IngestData(args)  # .pipeline()
+    ingestion_pipeline.create_schema()
+    ingestion_pipeline.pipeline(ingestion_pipeline.df_iter)
