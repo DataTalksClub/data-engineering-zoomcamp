ed23074f6810a399273cb6589b035b462a5fcf5f
dimkaart <dmitri.artjuch@tu-dortmund.de>
2023-01-17 22:27:06 +0100

    DTC_DEZ Video 1.2.1

---
+++ own_tasks_and_files/week_1/pipeline.py
@@ -0,0 +1,13 @@
+import sys
+
+import pandas as pd
+
+# print CLI arguments that are passed to the script
+print(sys.argv)
+
+day = sys.argv[1]
+
+# some fancy stuff with pandas
+
+print(f'job finished successfully for day = {day}')
+
