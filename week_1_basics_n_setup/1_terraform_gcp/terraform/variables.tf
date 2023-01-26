123e1eaa9f06f8faa6dd67683ea4dc77d3e16f5b
datadisciple <sean.meehan22@gmail.com>
2023-01-26 15:05:05 -0700

    updates region to be more local

--- week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
+++ week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
@@ -8,7 +8,7 @@ variable "project" {
 
 variable "region" {
   description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
-  default = "europe-west6"
+  default = "us-west1"
   type = string
 }
 
