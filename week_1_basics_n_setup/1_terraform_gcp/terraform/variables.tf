db199d8c0f98bea9ae03a2cb34c011de84c010b6
Brandon Bertelsen <brandon@bertelsen.ca>
2023-01-26 23:46:26 -0500

    Updates to variables.tf

--- week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
+++ week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
@@ -3,12 +3,12 @@ locals {
 }
 
 variable "project" {
-  description = "Your GCP Project ID"
+  description = "dtc-de-course-375921"
 }
 
 variable "region" {
   description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
-  default = "europe-west6"
+  default = "northamerica-northeast2"
   type = string
 }
 
