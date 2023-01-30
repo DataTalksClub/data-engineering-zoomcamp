20f6c40edb163331762a3e1e20e197f74bb24c6e
Georgy Petrukhin <mumbasa@gmail.com>
2023-01-30 12:03:31 +0000

    Terraform changes

--- week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
+++ week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
@@ -3,12 +3,12 @@ locals {
 }
 
 variable "project" {
-  description = "Your GCP Project ID"
+  description = "de-trainig"
 }
 
-variable "region" {
+variable "region" { 
   description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
-  default = "europe-west6"
+  default = "us-central1"
   type = string
 }
 
@@ -21,4 +21,10 @@ variable "BQ_DATASET" {
   description = "BigQuery Dataset that raw data (from GCS) will be written to"
   type = string
   default = "trips_data_all"
+}
+
+variable "TABLE_NAME" {
+  description = "BigQuery Table"
+  type = string
+  default = "ny_trips"   
 }
