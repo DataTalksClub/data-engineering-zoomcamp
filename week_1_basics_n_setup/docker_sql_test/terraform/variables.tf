da5758487db039ad4e1ed74cf8798586f8f32850
felipe buongermino <felipe.buongermino@gmail.com>
2023-02-05 18:15:12 -0300

    Terraform configuration

---
+++ week_1_basics_n_setup/docker_sql_test/terraform/variables.tf
@@ -0,0 +1,26 @@
+locals {
+  data_lake_bucket = "dtc_data_lake"
+}
+
+variable "project" {
+  description = "Your GCP Project ID"
+  default = "nytaxi-376623"
+  type = string
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "southamerica-east1"
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "trips_data_all"
+}
