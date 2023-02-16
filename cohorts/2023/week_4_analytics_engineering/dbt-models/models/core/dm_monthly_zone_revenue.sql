c024b7649b819e857c7d696fe07012490f07597a
MichalGasiorowski <MGasiorowski82@gmail.com>
2023-02-16 17:18:26 +0100

    copy dbt_models files from course

---
+++ cohorts/2023/week_4_analytics_engineering/dbt-models/models/core/dm_monthly_zone_revenue.sql
@@ -0,0 +1,31 @@
+{{ config(materialized='table') }}
+
+with trips_data as (
+    select * from {{ ref('fact_trips') }}
+)
+    select 
+    -- Reveneue grouping 
+    pickup_zone as revenue_zone,
+    date_trunc('month', pickup_datetime) as revenue_month, 
+    --Note: For BQ use instead: date_trunc(pickup_datetime, month) as revenue_month, 
+
+    service_type, 
+
+    -- Revenue calculation 
+    sum(fare_amount) as revenue_monthly_fare,
+    sum(extra) as revenue_monthly_extra,
+    sum(mta_tax) as revenue_monthly_mta_tax,
+    sum(tip_amount) as revenue_monthly_tip_amount,
+    sum(tolls_amount) as revenue_monthly_tolls_amount,
+    sum(ehail_fee) as revenue_monthly_ehail_fee,
+    sum(improvement_surcharge) as revenue_monthly_improvement_surcharge,
+    sum(total_amount) as revenue_monthly_total_amount,
+    sum(congestion_surcharge) as revenue_monthly_congestion_surcharge,
+
+    -- Additional calculations
+    count(tripid) as total_monthly_trips,
+    avg(passenger_count) as avg_montly_passenger_count,
+    avg(trip_distance) as avg_montly_trip_distance
+
+    from trips_data
+    group by 1,2,3
