acc211182a921d0ff414b881acb1711bebe66bc8
JMGGarcia <JMGGarcia@users.noreply.github.com>
2023-02-28 19:49:38 +0000

    Updated download_data.sh file for week 05 (#338)
    * updated download_data.sh for week 05
    
    Updated to use new data repo and correct file extensions
    
    * Update download_data.sh

--- week_5_batch_processing/code/download_data.sh
+++ week_5_batch_processing/code/download_data.sh
@@ -4,21 +4,19 @@ set -e
 TAXI_TYPE=$1 # "yellow"
 YEAR=$2 # 2020
 
-URL_PREFIX="https://s3.amazonaws.com/nyc-tlc/trip+data"
+URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
 
 for MONTH in {1..12}; do
   FMONTH=`printf "%02d" ${MONTH}`
 
-  URL="${URL_PREFIX}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv"
+  URL="${URL_PREFIX}/${TAXI_TYPE}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
 
   LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
-  LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv"
+  LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv.gz"
   LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
 
   echo "donwloading ${URL} to ${LOCAL_PATH}"
   mkdir -p ${LOCAL_PREFIX}
   wget ${URL} -O ${LOCAL_PATH}
 
-  echo "compressing ${LOCAL_PATH}"
-  gzip ${LOCAL_PATH}
 done
