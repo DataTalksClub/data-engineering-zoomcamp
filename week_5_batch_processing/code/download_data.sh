304af5819036782a9e1a9e388cc3fe9446b93e68
JMGGarcia <JMGGarcia@users.noreply.github.com>
2023-02-28 11:10:48 +0000

    Update download_data.sh

--- week_5_batch_processing/code/download_data.sh
+++ week_5_batch_processing/code/download_data.sh
@@ -4,12 +4,12 @@ set -e
 TAXI_TYPE=$1 # "yellow"
 YEAR=$2 # 2020
 
-URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/"
+URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
 
 for MONTH in {1..12}; do
   FMONTH=`printf "%02d" ${MONTH}`
 
-  URL="${URL_PREFIX}/${TAXI_TYPE}//${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
+  URL="${URL_PREFIX}/${TAXI_TYPE}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
 
   LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
   LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv.gz"
