d73325bd888bfc74656ca2855515b15fb294d677
Felipe Buongermino <felipe.buongermino@gmail.com>
2023-03-09 01:23:24 +0000

    dbt test

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,5 +1,13 @@
     {{config(materialized='view')}}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','green') }}
+  where vendorid is not null 
+)
+
 select
     {{ dbt_utils.surrogate_key(['vendorid','lpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
@@ -30,8 +38,9 @@ select
     {{ get_payment_type_description('payment_type')}} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{ source('staging','green') }}
-where vendorid is not null
+from tripdata
+where rn = 1
+
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
 
--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -1,5 +1,13 @@
     {{config(materialized='view')}}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+  from {{ source('staging','yellow') }}
+  where vendorid is not null 
+)
+
 select
     {{ dbt_utils.surrogate_key(['vendorid','tpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
@@ -31,8 +39,9 @@ select
     {{ get_payment_type_description('payment_type')}} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{ source('staging','yellow') }}
-where vendorid is not null
+from tripdata
+where rn = 1
+
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
 
