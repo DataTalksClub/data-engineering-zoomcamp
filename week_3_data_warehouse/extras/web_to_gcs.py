b473e9a41dc1559436b9b7ce104485812779b68c
JMGGarcia <JMGGarcia@users.noreply.github.com>
2023-04-12 16:36:40 +0100

    Updated code in week 3 to use new file locations

--- week_3_data_warehouse/extras/web_to_gcs.py
+++ week_3_data_warehouse/extras/web_to_gcs.py
@@ -2,7 +2,6 @@ import io
 import os
 import requests
 import pandas as pd
-import pyarrow
 from google.cloud import storage
 
 """
@@ -13,7 +12,7 @@ Pre-reqs:
 """
 
 # services = ['fhv','green','yellow']
-init_url = 'https://nyc-tlc.s3.amazonaws.com/trip+data/'
+init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
 # switch out the bucketname
 BUCKET = os.environ.get("GCP_GCS_BUCKET", "dtc-data-lake-bucketname")
 
@@ -40,18 +39,18 @@ def web_to_gcs(year, service):
         month = '0'+str(i+1)
         month = month[-2:]
 
-        # csv file_name 
-        file_name = service + '_tripdata_' + year + '-' + month + '.csv'
+        # csv file_name
+        file_name = f"{service}_tripdata_{year}-{month}.csv.gz"
 
         # download it using requests via a pandas df
-        request_url = init_url + file_name
+        request_url = f"{init_url}{service}/{file_name}"
         r = requests.get(request_url)
-        pd.DataFrame(io.StringIO(r.text)).to_csv(file_name)
+        open(file_name, 'wb').write(r.content)
         print(f"Local: {file_name}")
 
         # read it back into a parquet file
-        df = pd.read_csv(file_name)
-        file_name = file_name.replace('.csv', '.parquet')
+        df = pd.read_csv(file_name, compression='gzip')
+        file_name = file_name.replace('.csv.gz', '.parquet')
         df.to_parquet(file_name, engine='pyarrow')
         print(f"Parquet: {file_name}")
 
@@ -64,3 +63,4 @@ web_to_gcs('2019', 'green')
 web_to_gcs('2020', 'green')
 # web_to_gcs('2019', 'yellow')
 # web_to_gcs('2020', 'yellow')
+
