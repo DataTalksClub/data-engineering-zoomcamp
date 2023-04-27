ea6432f63bb1ffd4d0073e3491784594a8f897dd
uzquiano.tomas.usa@gmail.com <uzquiano.tomas.usa@gmail.com>
2023-04-27 23:27:26 +0000

    Dbt-init

--- week_6_stream_processing/streams/taxi_rides.py
+++
@@ -1,9 +0,0 @@
-import faust
-
-
-class TaxiRide(faust.Record, validation=True):
-    vendorId: str
-    passenger_count: int
-    trip_distance: float
-    payment_type: int
-    total_amount: float
