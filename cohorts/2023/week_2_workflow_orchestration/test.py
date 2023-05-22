a1bf110080f2285375db9f0e3bb16acdf6bd5a67
gluonhiggs <love.fiziks@gmail.com>
2023-05-22 12:44:29 +0700

    test

--- cohorts/2023/week_2_workflow_orchestration/test.py
+++ cohorts/2023/week_2_workflow_orchestration/test.py
@@ -26,3 +26,15 @@ def fetch_data(dataset_url:str)->pd.DataFrame:
       raise Exception(f"Could not fetch data from {dataset_url}")
 a = fetch_data(dataset_url).info()
 print(a)
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
