ac8d6faaed3b0ef7e18639312f1675808176f47d
thaivd2 <thaivd2@tpb.com.vn>
2023-06-16 17:58:59 +0700

    add dbt branch

--- week_6_stream_processing/java/kafka_examples/src/main/java/org/example/Topics.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Topics {
-    public static final String INPUT_RIDE_TOPIC = "rides";
-    public static final String INPUT_RIDE_LOCATION_TOPIC = "rides_location";
-    public static final String OUTPUT_TOPIC = "vendor_info";
-}
