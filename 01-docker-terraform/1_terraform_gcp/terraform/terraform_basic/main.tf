ec2212a7043d82c78401245a8a81c6d68b4d3b32
AdelElfar97 <100134120+AdelElfar97@users.noreply.github.com>
2024-01-26 21:42:45 -0400

    homework week1

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
@@ -2,47 +2,44 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.51.0"
+      version = "5.13.0"
     }
   }
 }
 
 provider "google" {
-# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
-#  credentials = 
-  project = "<Your Project ID>"
-  region  = "us-central1"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = "us-central1"
 }
 
+resource "google_storage_bucket" "demo-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
 
-
-resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "<Your Unique Bucket Name>"
-  location      = "US"
-
-  # Optional, but recommended settings:
-  storage_class = "STANDARD"
-  uniform_bucket_level_access = true
-
-  versioning {
-    enabled     = true
-  }
+  # lifecycle_rule {
+  #   condition {
+  #     age = 3
+  #   }
+  #   action {
+  #     type = "Delete"
+  #   }
+  # }
 
   lifecycle_rule {
-    action {
-      type = "Delete"
-    }
     condition {
-      age = 30  // days
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
     }
   }
-
-  force_destroy = true
 }
 
 
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "<The Dataset Name You Want to Use>"
-  project    = "<Your Project ID>"
-  location   = "US"
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
