d8b9c88f7f4aa69053e2b9051bc7d7e3a602a23b
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2024-01-28 13:06:28 +0200

    Create main.tf

---
+++ HW/  01-docker-terraform/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+
+resource "google_storage_bucket" "tf1-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+
+
+resource "google_bigquery_dataset" "tf1_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
