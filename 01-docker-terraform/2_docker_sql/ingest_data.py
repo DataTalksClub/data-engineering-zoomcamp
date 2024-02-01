e4a88e29e56297f7136e7018beb5bf31785bc511
Ibrahim <zemb34@gmail.com>
2024-02-01 02:04:12 -0400

    " Updated Data ingestion files"

--- 01-docker-terraform/2_docker_sql/ingest_data.py
+++ 01-docker-terraform/2_docker_sql/ingest_data.py
@@ -3,9 +3,7 @@
 
 import os
 import argparse
-
-from time import time
-
+import time
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -19,18 +17,22 @@ def main(params):
     table_name = params.table_name
     url = params.url
     
-    # the backup files are gzipped, and it's important to keep the correct extension
-    # for pandas to be able to open the file
-    if url.endswith('.csv.gz'):
-        csv_name = 'output.csv.gz'
-    else:
-        csv_name = 'output.csv'
+    # Check if the file is gzip-compressed
+    is_gzip = url.endswith('.gz')
 
-    os.system(f"wget {url} -O {csv_name}")
+    # Extracting the file name from the URL
+    file_name = url.split('/')[-1]
+
+    # Download the file
+    os.system(f"wget {url} -O {file_name}")
+
+    # Use pd.read_csv with compression parameter based on the file type
+    if is_gzip:
+        df_iter = pd.read_csv(file_name, compression='gzip', iterator=True, chunksize=100000)
+    else:
+        df_iter = pd.read_csv(file_name, iterator=True, chunksize=100000)
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
     df = next(df_iter)
 
@@ -41,26 +43,26 @@ def main(params):
 
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
-
-    while True: 
+    while True:
+        t_start = time.time()
 
         try:
-            t_start = time()
-            
             df = next(df_iter)
-
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-            df.to_sql(name=table_name, con=engine, if_exists='append')
-
-            t_end = time()
-
-            print('inserted another chunk, took %.3f second' % (t_end - t_start))
-
         except StopIteration:
             print("Finished ingesting data into the postgres database")
             break
+
+        df[['tpep_pickup_datetime', 'tpep_dropoff_datetime']] = df[['tpep_pickup_datetime', 'tpep_dropoff_datetime']].apply(pd.to_datetime)
+
+        df.to_sql(name= table_name, con=engine, if_exists='append')
+
+        t_end = time.time()
+
+        print(f'Inserted another chunk, took {t_end - t_start:.3f} seconds')
+
+    # Remove the downloaded file to save sapce
+    os.remove(file_name)
+    print(f"Downloaded file {file_name} has been removed.")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
