9d6f3365ed758bae1a77e6774c058b42733e65c3
Victoria Perez Mola <4315804+Victoriapm@users.noreply.github.com>
2024-02-04 20:48:14 +0100

    remove unused files

--- 04-analytics-engineering/taxi_rides_ny/models/core/dm_monthly_zone_revenue.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/dm_monthly_zone_revenue.sql
@@ -18,7 +18,6 @@ with trips_data as (
     sum(ehail_fee) as revenue_monthly_ehail_fee,
     sum(improvement_surcharge) as revenue_monthly_improvement_surcharge,
     sum(total_amount) as revenue_monthly_total_amount,
-    sum(congestion_surcharge) as revenue_monthly_congestion_surcharge,
 
     -- Additional calculations
     count(tripid) as total_monthly_trips,
