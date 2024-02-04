26ad97ed5d5b03634ae13cdd026acde744579c4f
Victoria Perez Mola <4315804+Victoriapm@users.noreply.github.com>
2024-02-04 22:47:16 +0100

    Update fact_trips.sql

--- 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
@@ -53,4 +53,4 @@ from trips_unioned
 inner join dim_zones as pickup_zone
 on trips_unioned.pickup_locationid = pickup_zone.locationid
 inner join dim_zones as dropoff_zone
-on trips_unioned.pickup_locationid = dropoff_zone.locationid
+on trips_unioned.dropoff_locationid = dropoff_zone.locationid
