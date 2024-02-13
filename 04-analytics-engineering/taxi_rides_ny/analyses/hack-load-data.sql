c9fae602b4a3f1ae689f78fd002b5808a974f27a
Victoria Perez Mola <4315804+Victoriapm@users.noreply.github.com>
2024-02-13 16:51:11 +0100

    Update hack-load-data.sql

--- 04-analytics-engineering/taxi_rides_ny/analyses/hack-load-data.sql
+++ 04-analytics-engineering/taxi_rides_ny/analyses/hack-load-data.sql
@@ -1,4 +1,6 @@
 -- MAKE SURE YOU REPLACE taxi-rides-ny-339813-412521 WITH THE NAME OF YOUR DATASET! 
+-- When you run the query, only run 5 of the ALTER TABLE statements at one time (by highlighting only 5). 
+-- Otherwise BigQuery will say too many alterations to the table are being made.
 
 CREATE TABLE  `taxi-rides-ny-339813-412521.trips_data_all.green_tripdata` as
 SELECT * FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2019`; 
