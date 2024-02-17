94aaf0f69af2909912f2e8015412621cce17e71e
ellacharmed <ellacharmed@gmail.com>
2024-02-17 13:00:12 +0000

    Build fact_trips success at end of 4.3.1

---
+++ cohorts/2024/de-lessons/taxi_rides_ny/models/core/dim_zones.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='table') }}
+
+select 
+    locationid, 
+    borough, 
+    zone, 
+    replace(service_zone,'Boro','Green') as service_zone 
+from {{ ref('taxi_zone_lookup') }}
