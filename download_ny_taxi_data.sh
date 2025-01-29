dea6c6ea88fc180c2f8e6d65bed35bccea305887
vanalex <apjorquera@gmail.com>
2025-01-29 11:59:27 +0100

    add dockerfile and script to download taxi data

---
+++ download_ny_taxi_data.sh
@@ -0,0 +1,2 @@
+wget https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-10.csv.gz
+wget https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv
