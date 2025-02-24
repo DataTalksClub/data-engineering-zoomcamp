289d86d3586b5849e7c80af49f8fd519bb4bf411
Jay Knowles <knowlesjr95@gmail.com>
2025-02-24 19:19:24 +0000

    sync2

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -8,7 +8,7 @@ with tripdata as
 (
   select *,
     row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
-  from {{ source('staging','green_tripdata') }}
+  from `kestrademo.de_zoomcamp.green_tripdata`
   where vendorid is not null 
 )
 select
--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -4,7 +4,7 @@ with tripdata as
 (
   select *,
     row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
-  from {{ source('staging','yellow_tripdata') }}
+  from `kestrademo.de_zoomcamp.yellow_tripdata`
   where vendorid is not null 
 )
 select
