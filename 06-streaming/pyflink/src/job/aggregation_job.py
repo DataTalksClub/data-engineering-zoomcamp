871829569940c5ba9d289e790285bd270756feed
Zach Wilson <zachwilson130@gmail.com>
2025-03-06 20:30:47 -0800

    adding pyflink (#715)

---
+++ 06-streaming/pyflink/src/job/aggregation_job.py
@@ -0,0 +1,90 @@
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import EnvironmentSettings, DataTypes, TableEnvironment, StreamTableEnvironment
+from pyflink.common.watermark_strategy import WatermarkStrategy
+from pyflink.common.time import Duration
+
+def create_events_aggregated_sink(t_env):
+    table_name = 'processed_events_aggregated'
+    sink_ddl = f"""
+        CREATE TABLE {table_name} (
+            event_hour TIMESTAMP(3),
+            test_data INT,
+            num_hits BIGINT,
+            PRIMARY KEY (event_hour, test_data) NOT ENFORCED
+        ) WITH (
+            'connector' = 'jdbc',
+            'url' = 'jdbc:postgresql://postgres:5432/postgres',
+            'table-name' = '{table_name}',
+            'username' = 'postgres',
+            'password' = 'postgres',
+            'driver' = 'org.postgresql.Driver'
+        );
+        """
+    t_env.execute_sql(sink_ddl)
+    return table_name
+
+def create_events_source_kafka(t_env):
+    table_name = "events"
+    source_ddl = f"""
+        CREATE TABLE {table_name} (
+            test_data INTEGER,
+            event_timestamp BIGINT,
+            event_watermark AS TO_TIMESTAMP_LTZ(event_timestamp, 3),
+            WATERMARK for event_watermark as event_watermark - INTERVAL '1' SECOND
+        ) WITH (
+            'connector' = 'kafka',
+            'properties.bootstrap.servers' = 'redpanda-1:29092',
+            'topic' = 'test-topic',
+            'scan.startup.mode' = 'earliest-offset',
+            'properties.auto.offset.reset' = 'earliest',
+            'format' = 'json'
+        );
+        """
+    t_env.execute_sql(source_ddl)
+    return table_name
+
+
+def log_aggregation():
+    # Set up the execution environment
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.enable_checkpointing(10 * 1000)
+    env.set_parallelism(3)
+
+    # Set up the table environment
+    settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+    t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+
+    watermark_strategy = (
+        WatermarkStrategy
+        .for_bounded_out_of_orderness(Duration.of_seconds(5))
+        .with_timestamp_assigner(
+            # This lambda is your timestamp assigner:
+            #   event -> The data record
+            #   timestamp -> The previously assigned (or default) timestamp
+            lambda event, timestamp: event[2]  # We treat the second tuple element as the event-time (ms).
+        )
+    )
+    try:
+        # Create Kafka table
+        source_table = create_events_source_kafka(t_env)
+        aggregated_table = create_events_aggregated_sink(t_env)
+
+        t_env.execute_sql(f"""
+        INSERT INTO {aggregated_table}
+        SELECT
+            window_start as event_hour,
+            test_data,
+            COUNT(*) AS num_hits
+        FROM TABLE(
+            TUMBLE(TABLE {source_table}, DESCRIPTOR(event_watermark), INTERVAL '1' MINUTE)
+        )
+        GROUP BY window_start, test_data;
+        
+        """).wait()
+
+    except Exception as e:
+        print("Writing records from Kafka to JDBC failed:", str(e))
+
+
+if __name__ == '__main__':
+    log_aggregation()
