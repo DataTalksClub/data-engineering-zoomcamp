e4b84917c5451ee4b091b6867973bac56ef72196
JoChuan Chou <r08725008@ntu.edu.tw>
2025-06-07 22:00:11 -0400

    update var

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "<Path to your Service Account json file>"
+  default     = "/Users/rosechou/Coding/DataEngineering/cogent-wall-462217-u4-8760e791ffe9.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
@@ -32,7 +32,7 @@ variable "bq_dataset_name" {
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "terraform-demo-terra-bucket"
+  default     = "terraform-demo-terra-bucket-rose"
 }
 
 variable "gcs_storage_class" {
