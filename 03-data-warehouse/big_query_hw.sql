3b047b0006cee454a6fa3a39bfb8ad2ee72e7a01
hnahtneyugn <tanguyenthanh1969@gmail.com>
2025-07-08 16:12:13 +0700

    finish week2 and week3

--- 03-data-warehouse/big_query_hw.sql
+++ 03-data-warehouse/big_query_hw.sql
@@ -1,30 +1,44 @@
-CREATE OR REPLACE EXTERNAL TABLE `taxi-rides-ny.nytaxi.fhv_tripdata`
+-- Homework for Data Engineering Zoomcamp week3, done in July 8th 2025
+CREATE OR REPLACE EXTERNAL TABLE `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata`
 OPTIONS (
-  format = 'CSV',
-  uris = ['gs://nyc-tl-data/trip data/fhv_tripdata_2019-*.csv']
+  format = 'PARQUET',
+  uris = ['gs://dtc_de_bucket/yellow_tripdata_2024/*.parquet']
 );
 
+-- Use this to create a regular/materialized table
+CREATE OR REPLACE TABLE `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata_regular` AS
+SELECT * FROM `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata`;
 
-SELECT count(*) FROM `taxi-rides-ny.nytaxi.fhv_tripdata`;
+-- Run these and verify that the results are equivalent!
+SELECT count(*) FROM `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata`;
+SELECT count(*) FROM `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata_regular`;
 
+-- Just hover over these 2 queries to see the estimated amount of data that will be read. Don't need to run these!
+SELECT COUNT(DISTINCT(PULocationID)) FROM `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata`;
+SELECT COUNT(DISTINCT(PULocationID)) FROM `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata_regular`;
 
-SELECT COUNT(DISTINCT(dispatching_base_num)) FROM `taxi-rides-ny.nytaxi.fhv_tripdata`;
+-- Similarly, just hover, don't run!
+SELECT PULocationID FROM `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata_regular`;
+SELECT PULocationID, DOLocationID FROM `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata_regular`;
 
+-- Run this!
+SELECT COUNT(fare_amount) 
+FROM `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata` 
+WHERE fare_amount = 0;
 
-CREATE OR REPLACE TABLE `taxi-rides-ny.nytaxi.fhv_nonpartitioned_tripdata`
-AS SELECT * FROM `taxi-rides-ny.nytaxi.fhv_tripdata`;
+-- Create a partitioned and clustered table!
+CREATE OR REPLACE TABLE `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata_partitioned_clustered`
+PARTITION BY DATE(tpep_dropoff_datetime)
+CLUSTER BY VendorID AS
+SELECT * FROM `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata_regular`;
 
-CREATE OR REPLACE TABLE `taxi-rides-ny.nytaxi.fhv_partitioned_tripdata`
-PARTITION BY DATE(dropoff_datetime)
-CLUSTER BY dispatching_base_num AS (
-  SELECT * FROM `taxi-rides-ny.nytaxi.fhv_tripdata`
-);
+-- Again, just hover over these two queries and see the estimation !!!
+SELECT COUNT(DISTINCT(VendorID))
+FROM `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata_regular`
+WHERE DATE(tpep_dropoff_datetime) BETWEEN '2024-03-01' AND '2024-03-15';
 
-SELECT count(*) FROM  `taxi-rides-ny.nytaxi.fhv_nonpartitioned_tripdata`
-WHERE DATE(dropoff_datetime) BETWEEN '2019-01-01' AND '2019-03-31'
-  AND dispatching_base_num IN ('B00987', 'B02279', 'B02060');
+SELECT COUNT(DISTINCT(VendorID))
+FROM `graphical-fort-463908-u4.dtc_de_dataset.yellow2024_tripdata_partitioned_clustered`
+WHERE DATE(tpep_dropoff_datetime) BETWEEN '2024-03-01' AND '2024-03-15';
 
-
-SELECT count(*) FROM `taxi-rides-ny.nytaxi.fhv_partitioned_tripdata`
-WHERE DATE(dropoff_datetime) BETWEEN '2019-01-01' AND '2019-03-31'
-  AND dispatching_base_num IN ('B00987', 'B02279', 'B02060');
+-- Congrats!
