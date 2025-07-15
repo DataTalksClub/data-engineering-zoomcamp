648ed038201a03371558a455b4a3f35351996a1d
Thanh Ta Nguyen <tanguyenthanh1969@gmail.com>
2025-07-15 09:51:32 +0000

    changes to staging files

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -8,7 +8,7 @@ with tripdata as
 (
   select *,
     row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
-  from {{ source('staging','green_tripdata') }}
+  from {{ source('staging','green_tripdata_2019_2020') }}
   where vendorid is not null 
 )
 select
--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -4,7 +4,7 @@ with tripdata as
 (
   select *,
     row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
-  from {{ source('staging','yellow_tripdata') }}
+  from {{ source('staging','yellow_tripdata2019_2020') }}
   where vendorid is not null 
 )
 select
