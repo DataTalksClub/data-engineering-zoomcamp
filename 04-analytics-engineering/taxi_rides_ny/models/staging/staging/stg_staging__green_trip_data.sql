06b395ba75b2619eea7bde645a06abf835d9d11b
Joye <joyelee21@gmail.com>
2025-08-01 21:58:25 +0000

    Folder name update

---
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/staging/stg_staging__green_trip_data.sql
@@ -0,0 +1,41 @@
+with 
+
+source as (
+
+    select * from {{ source('staging', 'green_trip_data') }}
+
+),
+
+renamed as (
+
+    select
+        vendor_id,
+        pickup_datetime,
+        dropoff_datetime,
+        store_and_fwd_flag,
+        rate_code,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        extra,
+        mta_tax,
+        tip_amount,
+        tolls_amount,
+        ehail_fee,
+        airport_fee,
+        total_amount,
+        payment_type,
+        distance_between_service,
+        time_between_service,
+        trip_type,
+        imp_surcharge,
+        pickup_location_id,
+        dropoff_location_id,
+        data_file_year,
+        data_file_month
+
+    from source
+
+)
+
+select * from renamed
