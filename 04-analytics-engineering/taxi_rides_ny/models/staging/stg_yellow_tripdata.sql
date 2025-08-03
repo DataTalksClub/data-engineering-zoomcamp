7d4006e00f6e748aecb3082c201547b7b42f7e9a
Joye <joyelee21@gmail.com>
2025-08-03 21:18:15 +0000

    Code merge for prod dep

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -1,24 +1,20 @@
-{{
-    config(
-        materialized='view'
-    )
-}}
-
+{{ config(materialized='view') }}
+ 
 with tripdata as 
 (
   select *,
     row_number() over(partition by vendor_id, pickup_datetime) as rn
-  from {{ source('staging','green_trip_data') }}
+  from {{ source('staging','yellow_trip_data') }}
   where vendor_id is not null 
 )
 select
-    -- identifiers
-    {{ dbt_utils.generate_surrogate_key(['vendor_id', 'pickup_datetime']) }} as tripid,
+   -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['vendor_id', 'pickup_datetime']) }} as tripid,    
     {{ dbt.safe_cast("vendor_id", api.Column.translate_type("integer")) }} as vendorid,
     {{ dbt.safe_cast("rate_code", api.Column.translate_type("integer")) }} as ratecodeid,
     {{ dbt.safe_cast("pickup_location_id", api.Column.translate_type("integer")) }} as pickup_locationid,
     {{ dbt.safe_cast("dropoff_location_id", api.Column.translate_type("integer")) }} as dropoff_locationid,
-    
+
     -- timestamps
     cast(pickup_datetime as timestamp) as pickup_datetime,
     cast(dropoff_datetime as timestamp) as dropoff_datetime,
@@ -27,24 +23,24 @@ select
     store_and_fwd_flag,
     {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
-    {{ dbt.safe_cast("trip_type", api.Column.translate_type("integer")) }} as trip_type,
-
+    -- yellow cabs are always street-hail
+    1 as trip_type,
+    
     -- payment info
     cast(fare_amount as numeric) as fare_amount,
     cast(extra as numeric) as extra,
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
-    cast(ehail_fee as numeric) as ehail_fee,
+    cast(0 as numeric) as ehail_fee,
     cast(imp_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
-    {{ get_payment_type_description("payment_type") }} as payment_type_description
+    {{ get_payment_type_description('payment_type') }} as payment_type_description
 from tripdata
 where rn = 1
 
-
--- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+-- dbt build --select <model.sql> --vars '{'is_test_run: false}'
 {% if var('is_test_run', default=true) %}
 
   limit 100
---
+++ 04-analytics-engineering/taxi_rides_ny/packages.yml
@@ -0,0 +1,5 @@
+packages:
+  - package: dbt-labs/dbt_utils
+    version: 1.3.0
+  - package: dbt-labs/codegen
+    version: 0.13.1
