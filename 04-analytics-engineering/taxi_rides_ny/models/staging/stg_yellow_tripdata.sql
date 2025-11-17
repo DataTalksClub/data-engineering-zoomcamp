9d0cb3eab8cfc6cde14d5ba9e218a9bc44c6ae09
Ziyue Song <songleanna0@gmail.com>
2025-11-17 00:28:13 +0000

    update staging files

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -2,8 +2,7 @@
  
 with tripdata as 
 (
-  select *,
-    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+  select *
   from {{ source('staging','yellow_tripdata') }}
   where vendorid is not null 
 )
@@ -38,7 +37,6 @@ select
     coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description
 from tripdata
-where rn = 1
 
 -- dbt build --select <model.sql> --vars '{'is_test_run: false}'
 {% if var('is_test_run', default=true) %}
