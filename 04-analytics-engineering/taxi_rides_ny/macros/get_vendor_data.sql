12c3516355a5bfdb1285f7e2effd5ba6c07b00ae
jmperafan <jmperafan@mail.com>
2026-01-12 23:47:16 +0100

    Modify get_vendor_data. It now returns just one column instead of a whole table

--- 04-analytics-engineering/taxi_rides_ny/macros/get_vendor_data.sql
+++ 04-analytics-engineering/taxi_rides_ny/macros/get_vendor_data.sql
@@ -1,13 +1,14 @@
 {#
-    Macro to generate vendor lookup data using Jinja dictionary.
+    Macro to generate vendor_name column using Jinja dictionary.
 
     This approach works seamlessly across BigQuery, DuckDB, Snowflake, etc.
-    by generating UNION ALL statements at compile time.
+    by generating a CASE statement at compile time.
 
-    Returns: SQL that creates vendor rows with vendor_id and vendor_name
+    Usage: {{ get_vendor_data('vendor_id') }}
+    Returns: SQL CASE expression that maps vendor_id to vendor_name
 #}
 
-{% macro get_vendor_data() %}
+{% macro get_vendor_data(vendor_id_column) %}
 
 {% set vendors = {
     1: 'Creative Mobile Technologies',
@@ -15,9 +16,10 @@
     4: 'Unknown/Other'
 } %}
 
-{% for vendor_id, vendor_name in vendors.items() %}
-    select {{ vendor_id }} as vendor_id, '{{ vendor_name }}' as vendor_name
-    {% if not loop.last %}union all{% endif %}
-{% endfor %}
+case {{ vendor_id_column }}
+    {% for vendor_id, vendor_name in vendors.items() %}
+    when {{ vendor_id }} then '{{ vendor_name }}'
+    {% endfor %}
+end
 
 {% endmacro %}
