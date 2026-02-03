5219b6067301b3a121aecb92a1dc84f35e3c378a
Muhammad Asadullah Al Ghozi <78595805+bunchofcodes@users.noreply.github.com>
2026-02-03 07:52:35 +0000

    update

---
+++ main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+#  credentials = 
+  project = "dte-de-486307"
+  region  = "us-central1"
+}
+
+
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "dte-de-486307-terra-bucket"
+  location      = "US"
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "<The Dataset Name You Want to Use>"
+  project    = "<Your Project ID>"
+  location   = "US"
+}
